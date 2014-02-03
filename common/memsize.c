636807fcf23bf3c56f286914d078e85dc64109f3
root <root@ubuntu.(none)>
2014-02-03 02:04:53 -0800

    first commit

---
+++ common/memsize.c
@@ -0,0 +1,78 @@
+/*
+ * (C) Copyright 2004
+ * Wolfgang Denk, DENX Software Engineering, wd@denx.de.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <config.h>
+#ifdef __PPC__
+/*
+ * At least on G2 PowerPC cores, sequential accesses to non-existent
+ * memory must be synchronized.
+ */
+# include <asm/io.h>	/* for sync() */
+#else
+# define sync()		/* nothing */
+#endif
+
+/*
+ * Check memory range for valid RAM. A simple memory test determines
+ * the actually available RAM size between addresses `base' and
+ * `base + maxsize'.
+ */
+unsigned long get_ram_size(unsigned long *base, unsigned long maxsize)
+{
+	volatile unsigned long *addr;
+	unsigned long           save[32];
+	unsigned long           cnt;
+	unsigned long           val;
+	unsigned long           size;
+	int                     i = 0;
+
+	for (cnt = (maxsize / sizeof (unsigned long)) >> 1; cnt > 0; cnt >>= 1) {
+		addr = base + cnt;	/* pointer arith! */
+		sync ();
+		save[i++] = *addr;
+		sync ();
+		*addr = ~cnt;
+	}
+
+	addr = base;
+	sync ();
+	save[i] = *addr;
+	sync ();
+	*addr = 0;
+
+	sync ();
+	if ((val = *addr) != 0) {
+		/* Restore the original data before leaving the function.
+		 */
+		sync ();
+		*addr = save[i];
+		for (cnt = 1; cnt < maxsize / sizeof(unsigned long); cnt <<= 1) {
+			addr  = base + cnt;
+			sync ();
+			*addr = save[--i];
+		}
+		return (0);
+	}
+
+	for (cnt = 1; cnt < maxsize / sizeof (unsigned long); cnt <<= 1) {
+		addr = base + cnt;	/* pointer arith! */
+		val = *addr;
+		*addr = save[--i];
+		if (val != ~cnt) {
+			size = cnt * sizeof (unsigned long);
+			/* Restore the original data before leaving the function.
+			 */
+			for (cnt <<= 1; cnt < maxsize / sizeof (unsigned long); cnt <<= 1) {
+				addr  = base + cnt;
+				*addr = save[--i];
+			}
+			return (size);
+		}
+	}
+
+	return (maxsize);
+}
